39a158eebc1e3e7fcad5fe4fb910a685f043bfe0
Kongleochi <54540110+Kongleochi@users.noreply.github.com>
2021-04-13 10:26:52 -0600

    Add files via upload

---
+++ contracts/prescriptionownership.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./prescriptionfactory.sol";
+import "./erc721.sol";
+import "./safemath.sol";
+
+contract PrescriptionOwnership is PrescriptionFactory, ERC721 {
+
+    using SafeMath for uint256;
+
+    mapping (uint => address) prescriptionApprovals;
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerPrescriptionCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        return prescriptionToOwner[_tokenId];
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerPrescriptionCount[_to] = ownerPrescriptionCount[_to].add(1);
+        ownerPrescriptionCount[msg.sender] = ownerPrescriptionCount[msg.sender].sub(1);
+        prescriptionToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require (prescriptionToOwner[_tokenId] == msg.sender || prescriptionApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
+    }
+
+    function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+        prescriptionApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
+    }
+
+}
---
+++ contracts/prescriptionquery.sol
@@ -0,0 +1,35 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./safemath.sol";
+import "./prescriptionownership.sol";
+
+contract PrescriptionQuery is PrescriptionOwnership{
+
+    using SafeMath for uint256;
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerPrescriptionCount[_owner];
+    }
+
+    function findWithId(uint256 _tokenId) external view returns (string memory) {
+        return prescriptions[_tokenId].name;// not sure if this is correct
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerPrescriptionCount[_to] = ownerPrescriptionCount[_to].add(1);
+        ownerPrescriptionCount[msg.sender] = ownerPrescriptionCount[msg.sender].sub(1);
+        prescriptionToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require (prescriptionToOwner[_tokenId] == msg.sender || prescriptionApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
+    }
+
+    function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+        prescriptionApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
+    }
+
+}
